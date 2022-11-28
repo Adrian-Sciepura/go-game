f69dd7cb9b3b93ee4e24259cf42d68ec29b6302e
Adrian Ściepura <adriansciepura@outlook.com>
2022-11-28 23:39:47 +0100

    Added libraries

---
+++ go-game/main.cpp
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include"conio2.h"
+
+
+
+void display_menu(int start_x, int start_y)
+{
+
+}
+
+
+int main() 
+{
+
+#ifndef __cplusplus
+	Conio2_Init();
+#endif
+
+
+
+
+	return 0;
+}
