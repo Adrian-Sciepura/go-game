4f22924421222bfa68eae5ff8076d8258188553e
Adrian Ściepura <adriansciepura@outlook.com>
2022-12-08 16:32:19 +0100

    Moved stone placing mechanics to Game class

--- go-game/Game.h
+++ go-game/Game.h
@@ -14,10 +14,121 @@ private:
 	Legend* legend;
 	Player* player1;
 	Player* player2;
+	Point* checked;
+	int how_many_checked;
 	char board_location;
 	int board_size;
 	bool lock;
 	bool handicap;
+
+	//GAME LOGIC
+
+	bool set_stone(Player*& player, Point p)
+	{
+		char value_of_position = board->get_value_by_pos(p);
+
+		if (value_of_position == field_type::INTERIOR)
+		{
+			board->set_value_by_pos(p, player->get_id());
+			if (is_suicide(player, p))
+			{
+				board->set_value_by_pos(p, field_type::INTERIOR);
+				return false;
+			}
+
+			player->score += can_beat(player, p);
+			return true;
+		}
+		return false;
+	}
+
+	bool is_suicide(Player*& player, Point p)
+	{
+		if (has_even_one_empty_neighbour(p, player->get_id()))
+		{
+			clear_checked_list();
+			return false;
+		}
+		clear_checked_list();
+		return true;
+	}
+
+	int can_beat(Player*& player, Point p)
+	{
+		int beated = 0;
+		Point possibilities[4] =
+		{
+			{p.x - 1, p.y}, {p.x + 1, p.y},
+			{p.x, p.y + 1}, {p.x, p.y - 1}
+		};
+
+		for (int i = 0; i < 4; i++)
+		{
+			char opponent_id = 99 - player->get_id();
+			if (has_even_one_empty_neighbour(possibilities[i], opponent_id) == false)
+			{
+				remove_points();
+				beated = this->how_many_checked;
+				clear_checked_list();
+			}
+		}
+
+		return beated;;
+	}
+
+	bool has_even_one_empty_neighbour(Point p, char search)
+	{
+		Point possibilities[4] =
+		{
+			{p.x - 1, p.y}, {p.x + 1, p.y},
+			{p.x, p.y + 1}, {p.x, p.y - 1}
+		};
+
+		char current_element = board->get_value_by_pos(p);
+
+		if (current_element == field_type::INTERIOR)
+		{
+			return true;
+		}
+
+		if (current_element == search)
+		{
+			checked[how_many_checked] = p;
+			how_many_checked++;
+			for (int i = 0; i < 4; i++)
+			{
+				if (is_already_checked(possibilities[i]) == false)
+				{
+					if (has_even_one_empty_neighbour(possibilities[i], search))
+						return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	bool is_already_checked(Point point)
+	{
+		for (int i = 0; i < how_many_checked; i++)
+		{
+			if (point == checked[i])
+				return true;
+		}
+		return false;
+	}
+
+	void remove_points()
+	{
+		for (int i = 0; i < how_many_checked; i++)
+			board->set_value_by_pos(checked[i], field_type::INTERIOR);
+	}
+
+	void clear_checked_list()
+	{
+		for (int i = 0; i < how_many_checked; i++)
+			checked[i] = { -1, -1 };
+		how_many_checked = 0;
+	}
 
 public:
 	Game(int board_size, char board_location)
@@ -32,6 +143,8 @@ public:
 		this->legend = new Legend();
 		this->player1 = new Player(field_type::PLAYER_1);
 		this->player2 = new Player(field_type::PLAYER_2);
+		this->checked = new Point[board_size*board_size];
+		this->how_many_checked = 0;
 		setup_points();
 	}
 
@@ -42,6 +155,7 @@ public:
 		delete legend;
 		delete player1;
 		delete player2;
+		delete[] checked;
 	}
 
 	void setup_points()
@@ -67,6 +181,7 @@ public:
 
 	void display()
 	{
+		setup_points();
 		legend->display();
 		Helper::display_border(cursor->limit_1 - 1, { cursor->limit_2.x + 2, cursor->limit_2.y + 2 }, YELLOW);
 	}
@@ -124,6 +239,7 @@ public:
 
 		delete this->board;
 		this->board = new Board(size, tour);
+		this->board_size = size;
 
 		for (int y = 0; y < size; y++)
 		{
@@ -136,6 +252,10 @@ public:
 		}
 
 		fclose(file);
+
+		delete[] this->checked;
+		this->checked = new Point[board_size * board_size];
+
 		return true;
 	}
 
@@ -173,16 +293,16 @@ public:
 					bool success;
 					if (handicap)
 					{
-						success = player2->set_stone(*board, cursor->relative_pos);
+						success = set_stone(player1, cursor->relative_pos);
 						if (success)
 							lock = false;
 					}
 					else
 					{
 						if (board->tour)
-							success = player2->set_stone(*board, cursor->relative_pos);
+							success = set_stone(player2, cursor->relative_pos);
 						else
-							success = player1->set_stone(*board, cursor->relative_pos);
+							success = set_stone(player1, cursor->relative_pos);
 
 						if (success)
 						{
@@ -190,7 +310,6 @@ public:
 							lock = false;
 						}
 					}
-
 				}
 				break;
 			}
--- go-game/Player.h
+++ go-game/Player.h
@@ -5,7 +5,6 @@ class Player
 {
 private:
 	char id;
-
 public:
 	int score;
 
@@ -15,102 +14,10 @@ public:
 		this->score = 0;
 	}
 
-	bool has_even_one_empty_neighbour(Board& board, Point p, char search)
+	char get_id()
 	{
-		Point possibilities[4] =
-		{
-			{p.x - 1, p.y}, {p.x + 1, p.y},
-			{p.x, p.y + 1}, {p.x, p.y - 1}
-		};
-
-		char current_element = board.get_value_by_pos(p);
-
-		if (current_element == field_type::INTERIOR)
-		{
-			return true;
-		}
-		
-		if (current_element == search)
-		{
-			board.set_value_by_pos(p, '4');
-
-			for (int i = 0; i < 4; i++)
-			{
-				if (has_even_one_empty_neighbour(board, possibilities[i], search) == true)
-					return true;
-			}
-		}
-		return false;
-	}
-
-	bool is_suicide(Board& board, Point p)
-	{
-		if (has_even_one_empty_neighbour(board, p, this->id))
-		{
-			repair(board, this->id);
-			return false;
-		}	
-		repair(board, this->id);
-		return true;
-	}
-
-	int can_beat(Board& board, Point p)
-	{
-		int beated = 0;
-		Point possibilities[4] =
-		{
-			{p.x - 1, p.y}, {p.x + 1, p.y},
-			{p.x, p.y + 1}, {p.x, p.y - 1}
-		};
-
-		for (int i = 0; i < 4; i++)
-		{
-			char opponent_id = 99 - this->id;
-			if (has_even_one_empty_neighbour(board, possibilities[i], opponent_id) == false)
-				beated += repair(board, field_type::INTERIOR);
-			else
-				repair(board, opponent_id);
-		}
-
-		return beated;
-	}
-
-	int repair(Board& b, char player)
-	{
-		int counter = 0;
-		for (int y = 0; y < b.get_board_size(); y++)
-		{
-			for (int x = 0; x < b.get_board_size(); x++)
-			{
-				if (b.get_value_by_pos({ x,y }) == '4')
-				{
-					counter++;
-					b.set_value_by_pos({ x,y }, player);
-				}
-			}
-		}
-		return counter;
-	}
-
-	bool set_stone(Board& board, Point p)
-	{
-		char value_of_position = board.get_value_by_pos(p);
-
-		if (value_of_position == field_type::INTERIOR)
-		{
-			board.set_value_by_pos(p, this->id);
-			if (is_suicide(board, p))
-			{
-				board.set_value_by_pos(p, field_type::INTERIOR);
-				return false;
-			}
-
-			int beat = can_beat(board, p);
-			this->score += beat;
-
-			return true;
-		}
-		return false;
+		return this->id;
 	}
 };
+
 
