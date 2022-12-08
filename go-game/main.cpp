63f462e097656312654ed72b72aab0602c2faaa4
Adrian Ściepura <adriansciepura@outlook.com>
2022-12-08 02:17:01 +0100

    Moved game-related mechanics to Game class

--- go-game/main.cpp
+++ go-game/main.cpp
@@ -1,46 +1,22 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdlib.h>
 #include "conio2.h"
-#include "Legend.h"
-#include "Board.h"
-#include "Point.h"
 #include "Menu.h"
 #include "Cursor.h"
-#include "Player.h"
+#include "Game.h"
 
 #define DISTANCE 10
 
-char* file_selector(Cursor& cursor)
-{
-	Input_Menu file_selector{ cursor, "Enter file name", YELLOW };
-	file_selector.display();
-	char* buffer = NULL;
-	int input;
-
-	while (buffer == NULL)
-	{
-		input = getch();
-		buffer = file_selector.handle_input(input);
-	}
-
-	int i = 0;
-	char* result = new char[16];
-
-	return strcat(buffer, ".bin");
-}
-
 int size_selector()
 {
+	Cursor cursor;
+	_setcursortype(_NOCURSOR);
 	int input;
 	char* result = NULL;
 
-	Cursor cursor;
-
-	Selection_Menu_Element* elements = (Selection_Menu_Element*)malloc(4 * sizeof(Selection_Menu_Element));
-	elements[0] = { SELECTION, "9x9", "9" };
-	elements[1] = { SELECTION, "13x13", "13" };
-	elements[2] = { SELECTION, "19x19", "19" };
-	elements[3] = { INPUT, "Other" };
+	Selection_Menu_Element* elements = new Selection_Menu_Element[4]{ 
+		{ SELECTION, "9x9", "9" }, { SELECTION, "13x13", "13" }, { SELECTION, "19x19", "19" }, { INPUT, "Other" }
+	};
 
 	Selection_Menu board_size_selector
 	{
@@ -58,14 +34,38 @@ int size_selector()
 		result = board_size_selector.handle_input(input);
 	}
 
-	free(elements);
+	delete[] elements;
 	return Helper::convert_string_to_int(result);
 }
 
-void show_message(Cursor& cursor, const char* message)
+char* file_selector()
 {
-	Selection_Menu_Element* elements = (Selection_Menu_Element*)malloc(sizeof(Selection_Menu_Element));
-	elements[0] = { SELECTION, "OK", 0 };
+	Cursor cursor;
+	_setcursortype(_NOCURSOR);
+	Input_Menu file_selector{ cursor, "Enter file name", YELLOW };
+	file_selector.display();
+	char* buffer = NULL;
+	int input;
+
+	while (buffer == NULL)
+	{
+		input = getch();
+		buffer = file_selector.handle_input(input);
+	}
+
+	int i = 0;
+	char* result = new char[16];
+
+	return strcat(buffer, ".bin");
+}
+
+void show_message(const char* message)
+{
+	Cursor cursor;
+	Selection_Menu_Element* elements = new Selection_Menu_Element[1]
+	{
+		{ SELECTION, "OK", 0 }
+	};
 
 	Selection_Menu sm
 	{
@@ -73,152 +73,55 @@ void show_message(Cursor& cursor, const char* message)
 	};
 	sm.display();
 	getch();
-	free(elements);
-}
-
-void display(Board* board, Cursor& cursor, char board_location)
-{
-	Point menu_display_pos;
-	switch (board_location)
-	{
-		case 'r':
-		{
-			menu_display_pos.x = 2;
-			menu_display_pos.y = 2;
-
-			cursor.limit_1.x = Legend::get_longest_line_length() + DISTANCE+1;
-			cursor.limit_1.y = 3;
-
-			break;
-		}
-		case 'l':
-		{
-			menu_display_pos.x = board->get_board_size() + DISTANCE + 2;
-			menu_display_pos.y = 2;
-
-			cursor.limit_1.x = 2;
-			cursor.limit_1.y = 2;
-			break;
-		}
-	}
-
-	cursor.limit_2 = {cursor.limit_1.x + board->get_board_size()*2-2, cursor.limit_1.y + board->get_board_size()-1 };
-	Legend::display(menu_display_pos, cursor.relative_pos, board->tour);
-	board->display_area(cursor.limit_1);
-}
-
-void new_game(Board*& board, int board_size)
-{
-	if (board != NULL)
-	{
-		delete board;
-	}
-	board = new Board(board_size);
-}
-
-void init(Board*& board, int& board_size)
-{
-#ifndef __cplusplus
-	Conio2_Init();
-#endif
-	settitle("Adrian Sciepura 193350");
-	_setcursortype(_NOCURSOR);
-	board_size = size_selector();
-	new_game(board, board_size);
-}
-
-void refresh_all(Board*& board, Cursor& cursor, char board_location)
-{
-	display(board, cursor, board_location);
-	Helper::display_border(cursor.limit_1 - 1, { cursor.limit_2.x + 2, cursor.limit_2.y + 2 }, YELLOW);
-	cursor.absolute_pos = cursor.limit_1;
+	delete[] elements;
 }
 
 int main()
 {
-	Board* board = NULL;
-	Cursor cursor;
-	Player p1(Board::PLAYER_1);
-	Player p2(Board::PLAYER_2);
-	char board_location = 'r';
-	int board_size = 19;
+	Game game(size_selector(), 'r');
 	int input = 0;
-	bool lock = false;
-
-	init(board, board_size);
-	refresh_all(board, cursor, board_location);
+	game.display();
+	settitle("Adrian Sciepura 193350");
+	_setcursortype(_NOCURSOR);
 
 	while (input != buttons::q)
 	{
-		textcolor(WHITE);
-		display(board, cursor, board_location);
-		cursor.display();
+		game.update();
 		input = getch();
-
 		switch (input)
 		{
-			case 0:
-			{
-				if (lock == false)
-				{
-					input = getch();
-					cursor.move(input);
-				}
-				break;
-			}
-			case buttons::i:
-			{
-				lock = true;
-				break;
-			}
-			case buttons::ENTER:
-			{
-				bool success;
-				if (board->tour)
-					success = p2.set_stone(board, cursor.relative_pos);
-				else
-					success = p1.set_stone(board, cursor.relative_pos);
-
-				if (success)
-				{
-					board->tour = !board->tour;
-					lock = false;
-				}
-				break;
-			}
-			case buttons::ESC:
-			{
-				lock = false;
-				break;
-			}
 			case buttons::s:
 			{
-				char* file_name = file_selector(cursor);
-				if (board->save(file_name) == false)
+				char* file_name = file_selector();
+				if (game.save(file_name) == false)
 				{
-					show_message(cursor, "An error has occurred while saving the file");
+					show_message("An error has occurred while saving the file");
 				}
-				refresh_all(board, cursor, board_location);
+				game.display();
 				break;
 			}
 			case buttons::l:
 			{
-				char* file_name = file_selector(cursor);
-				if(board->load(file_name) == false)
+				char* file_name = file_selector();
+				if (game.load(file_name) == false)
 				{
-					show_message(cursor, "An error has occurred while reading the file");
+					show_message("An error has occurred while reading the file");
 				}
-				refresh_all(board, cursor, board_location);
+				game.display();
 				break;
 			}
 			case buttons::n:
 			{
-				board_size = board->get_board_size();
-				new_game(board, board_size);
-				refresh_all(board, cursor, board_location);
+				game.new_game();
+				break;
+			}
+			default:
+			{
+				game.handle_input(input);
 				break;
 			}
 		}
 	}
+
 	getch();
 }
